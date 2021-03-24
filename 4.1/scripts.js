924b8e724953d708dee50b4020585eac304e8fc6
Renan <renan.vamo@gmail.com>
2021-03-24 18:29:00 -0300

    Adiciona o ex3, comparacao de 3 numeros

--- 4.1/scripts.js
+++ 4.1/scripts.js
@@ -1,5 +1,6 @@
-let numA = 4;
-let numB = 2;
+let numA = 10;
+let numB = 9;
+let numC = 6;
 
 /* Exercício 1
 console.log(`A soma de ${numA} + ${numB} é igual a ${numA + numB}`);
@@ -9,6 +10,7 @@ console.log(`A divisão de ${numA} / ${numB} é igual a ${numA / numB}`);
 console.log(`O módulo de ${numA} % ${numB} é igual a ${numA % numB}`);
 */
 
+/* Exercício 2
 if (numA > numB) {
   console.log(`${numA} é maior que ${numB}`);
 } else if (numB > numA) {
@@ -16,3 +18,18 @@ if (numA > numB) {
 } else if (numA === numB){
   console.log(`${numA} é igual a ${numB}`);
 } 
+*/
+
+/* Exercicio 3*/
+if (numA > numB && numA > numC) {
+  console.log(`${numA} é maior que ${numB} e ${numC}`);
+} else if (numB > numA && numB > numC) {
+  console.log(`${numB} é maior que ${numA} e ${numC}`);
+} else if (numC > numA && numC > numB) {
+  console.log(`${numC} é maior que ${numA} e ${numB}`);
+} else {
+  console.log('Não há um valor maior que os outros dois ao mesmo tempo');
+}
+
+
+
