c7b8c0e9fc4e4ea6fb14567933a222b8cef41be7
Renan <renan.vamo@gmail.com>
2021-03-24 18:16:03 -0300

    Adiciona o arquivo de js e faz as 5 operacoes do exercicio 1

---
+++ 4.1/scripts.js
@@ -0,0 +1,12 @@
+let numA = 4
+let numB = 2
+
+console.log(`A soma de ${numA} + ${numB} é igual a ${numA + numB}`)
+
+console.log(`A subtração de ${numA} - ${numB} é igual a ${numA - numB}`)
+
+console.log(`A multiplicação de ${numA} * ${numB} é igual a ${numA * numB}`)
+
+console.log(`A divisão de ${numA} / ${numB} é igual a ${numA / numB}`)
+
+console.log(`O módulo de ${numA} % ${numB} é igual a ${numA % numB}`)
