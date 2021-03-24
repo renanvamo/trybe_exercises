6e4144151e8c0a5c6a41b9cb6be9cbee6eaaa5a5
Renan <renan.vamo@gmail.com>
2021-03-24 18:22:22 -0300

    Adiciona os requisitos do ex2, if/else

--- 4.1/scripts.js
+++ 4.1/scripts.js
@@ -1,12 +1,18 @@
-let numA = 4
-let numB = 2
+let numA = 4;
+let numB = 2;
 
-console.log(`A soma de ${numA} + ${numB} é igual a ${numA + numB}`)
+/* Exercício 1
+console.log(`A soma de ${numA} + ${numB} é igual a ${numA + numB}`);
+console.log(`A subtração de ${numA} - ${numB} é igual a ${numA - numB}`);
+console.log(`A multiplicação de ${numA} * ${numB} é igual a ${numA * numB}`);
+console.log(`A divisão de ${numA} / ${numB} é igual a ${numA / numB}`);
+console.log(`O módulo de ${numA} % ${numB} é igual a ${numA % numB}`);
+*/
 
-console.log(`A subtração de ${numA} - ${numB} é igual a ${numA - numB}`)
-
-console.log(`A multiplicação de ${numA} * ${numB} é igual a ${numA * numB}`)
-
-console.log(`A divisão de ${numA} / ${numB} é igual a ${numA / numB}`)
-
-console.log(`O módulo de ${numA} % ${numB} é igual a ${numA % numB}`)
+if (numA > numB) {
+  console.log(`${numA} é maior que ${numB}`);
+} else if (numB > numA) {
+  console.log(`${numB} é maior que ${numA}`);
+} else if (numA === numB){
+  console.log(`${numA} é igual a ${numB}`);
+} 
