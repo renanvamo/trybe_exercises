9025d46036e94eb3a38309829a0ef6a770647bf3
Renan <renan.vamo@gmail.com>
2021-03-25 15:39:41 -0300

    Realizado o ex 2, soma dos itens da array

--- 4.2/ex2.js
+++ 4.2/ex2.js
@@ -1,1 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+for (let number of numbers) {
+  //Mostra qual o valor da somatório até o momento e com qual número está sendo somado.
+  console.log(`${sum} + ${number}`);
+  sum += number;
+}
