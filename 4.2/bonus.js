e39d582594c9a314384692843776571f45fbebb7
Renan <renan.vamo@gmail.com>
2021-03-25 17:11:15 -0300

    Ex1 do bonus resolvido

---
+++ 4.2/bonus.js
@@ -0,0 +1,20 @@
+
+let array = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
+
+
+/* Exercicio 1*/
+
+for (let index = 1; index < array.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (array[index] < array[secondIndex]) {
+      let position = array[index];
+      array[index] = array[secondIndex];
+      array[secondIndex] = position;
+    }
+  }
+}
+console.log(array);
+
+/* Exercicio 2/*
+
+/* Exercicio 3/*
