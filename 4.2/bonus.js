df7387f4dd715091a4641c8a3e30b76c3b1d51c2
Renan <renan.vamo@gmail.com>
2021-03-25 17:27:12 -0300

    Resolve o ex2 do bonus

--- 4.2/bonus.js
+++ 4.2/bonus.js
@@ -2,7 +2,7 @@
 let array = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
 
 
-/* Exercicio 1*/
+/* Exercicio 1
 
 for (let index = 1; index < array.length; index += 1) {
   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
@@ -14,7 +14,20 @@ for (let index = 1; index < array.length; index += 1) {
   }
 }
 console.log(array);
+*/
 
-/* Exercicio 2/*
+/* Exercicio 2*/
 
-/* Exercicio 3/*
+for (let index = 1; index < array.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (array[index] > array[secondIndex]) {
+      let position = array[index];
+      array[index] = array[secondIndex];
+      array[secondIndex] = position;
+    }
+  }
+}
+console.log(array);
+
+
+/* Exercicio 3*/
