05970ddb89781cf2f4af1dbf7da7aa5b83653697
Renan <renan.vamo@gmail.com>
2021-03-25 15:51:22 -0300

    Ex 4 realizado

--- 4.2/ex4.js
+++ 4.2/ex4.js
@@ -1,1 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let average;
+for (let number of numbers) {
+  sum += number;
+} 
+average = (sum / numbers.length);
+
+if (average >= 20) {
+  console.log('A média é maior do que 20');
+} else if (average < 20) {
+  console.log('A média é menor do que 20')
+}
