8bf5414c89a4702fd89f7db29672370d2b259e0b
Renan <renan.vamo@gmail.com>
2021-03-27 10:00:51 -0300

    Corrige uma didiferença que exisita para o enunciado do ex

--- 4.3/ex4.js
+++ 4.3/ex4.js
@@ -1,36 +1,37 @@
 //4 - Por fim, faça uma pirâmide com n asteriscos de base:
+
 let number = Math.ceil(Math.random() * 15)
-console.log(`N = ${number}`);
+console.log(`Number = ${number}`);
 let inputLine = '';
 let symbol = '*';
 
-let counterDown = Math.ceil(number / 2);
-let counterUp = Math.ceil(number / 2);
-
+//Como o número é muito baixo para fazer uma pirâmide, aumentamos o valor de number
+if (number < 3) {
+  number += 4;
+  console.log(`Número muito baixo - O number foi alterado para = ${number}`);
+}
 //verificamos se o número é impar, se não for, o number vai diminuir 1 para o exerício funcionar como esperado
 if (number % 2 === 0) {
   number -= 1;
-  console.log(`O número foi alterado para ${number}`);
+  console.log(`Como o nṹmero é par - O número foi alterado para ${number}`);
 }
-for (let lineIndex = 1; lineIndex <= number; lineIndex += 1) {
+//Descobrimos qual a metade do number para usar de referência
+let counterDown = Math.ceil(number / 2);
+let counterUp = Math.ceil(number / 2);
+
+for (let lineIndex = 1; lineIndex <= number; lineIndex += 2) {
   for (let columnIndex = 1; columnIndex <= number; columnIndex += 1) { 
-    if (lineIndex % 2 === 1) {
-      if (columnIndex <= counterUp && columnIndex >= counterDown) {
-        inputLine += '*';
-      } else {
-        inputLine += ' ';
-      }
+    if (columnIndex <= counterUp && columnIndex >= counterDown) {
+      inputLine += symbol;
     } else {
       inputLine += ' ';
-    } 
+    }
   } 
   console.log(inputLine);
   inputLine = '';
-  //se for uma linha impar, os contadores se alteram, senão não
-  if (lineIndex % 2 === 1) {
   counterUp += 1;
   counterDown -= 1;
-  }
+  
 }
 
       
