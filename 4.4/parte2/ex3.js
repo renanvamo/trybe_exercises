eb1008c36176728a8b25305da7fac3708a0e599a
Renan <renan.vamo@gmail.com>
2021-03-29 19:55:38 -0300

    Resolve o ex3

---
+++ 4.4/parte2/ex3.js
@@ -0,0 +1,14 @@
+// 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
+myArray = [2, 4, 6, 7, 10, 0, -3];
+
+function verifySmallerNumber(array) {
+  let smallerNumber = array[0];
+    for (let index in array) {
+      if (array[index] < smallerNumber) {
+      smallerNumber = array[index];
+    }
+  }
+  return array.indexOf(smallerNumber);
+}
+console.log(verifySmallerNumber(myArray));
