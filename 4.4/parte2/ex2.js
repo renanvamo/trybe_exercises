6c3f67ca0c8e7e692641e3cfdb55da94b0be24c4
Renan <renan.vamo@gmail.com>
2021-03-29 19:51:25 -0300

    Arruma um detalhe do ex2

--- 4.4/parte2/ex2.js
+++ 4.4/parte2/ex2.js
@@ -4,7 +4,7 @@ let myArray = [2, 3, 6, 7, 10, 1];
 
 function verifyHigherNumber(array) {
   let higherNumber = 0;
-    for (let index = 0; index < array.length; index += 1) {
+    for (let index in array) {
       if (array[index] > higherNumber) {
       higherNumber = array[index];
     }
